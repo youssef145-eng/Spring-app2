a5d50502154a7e8ffd03a73471aeb18c20e536e7
youssef145-eng <yousefsameh174@gmail.com>
2025-04-13 20:25:27 +0200

    query parameter

--- src/main/java/org/example/springapp2/controller/TestController.java
+++ src/main/java/org/example/springapp2/controller/TestController.java
@@ -1,6 +1,7 @@
 package org.example.springapp2.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,8 +13,15 @@ public class TestController {
         return "Hello World";
     }
     @GetMapping("/search")
-    public String Search(@RequestParam String name, @RequestParam int age) {
+    public String Search(@RequestParam String name, @RequestParam int age
+    ,@RequestParam(required =false,name = "gen") String gender) {
 
-        return "Hello "+name+" "+age;
+        return "Hello "+name+" "+age+" "+gender;
+    }
+    @GetMapping("/testpath/{name}/{age}/{gender}")
+    public String testpath(@PathVariable String name, @PathVariable int age,
+                           @PathVariable String gender) {
+
+        return "name: "+name+"   age:"+age+"  gender: "+gender;
     }
 }
